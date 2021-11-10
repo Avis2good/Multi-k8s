0879611afb3a2ca467f7bbc21b20090cfd25f77d
Sidecar <sidecar@sidecars-mbp.myfiosgateway.com>
2021-11-10 14:38:47 -0500

    added deplotment script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker built -t avis2good/multi-client:latest -t avis2good/multi-client:$SHA -f ./client/Dockerfile ./client
+docker built -t avis2good/multi-server:latest -t avis2good/multi-server:$SHA -f ./server/Dockerfile ./server
+docker built -t avis2good/multi-worker:latest -t avis2good/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push avis2good/multi-client:latest 
+docker push avis2good/multi-server:latest 
+docker push avis2good/multi-worker:latest
+
+docker push avis2good/multi-client:$SHA
+docker push avis2good/multi-server:$SHA
+docker push avis2good/multi-worker:$SHA
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=avis2good/multi-server:$SHA
+kubectl set image deployments/client-deployment client=avis2good/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=avis2good/multi-worker:$SHA
