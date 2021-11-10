b1cca48ffc8ec46774da4b0073f3ce56fb856e31
Sidecar <sidecar@sidecars-mbp.myfiosgateway.com>
2021-11-10 15:04:55 -0500

    added deplotment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker built -t avis2good/multi-client:latest -t avis2good/multi-client:$SHA -f ./client/Dockerfile ./client
-docker built -t avis2good/multi-server:latest -t avis2good/multi-server:$SHA -f ./server/Dockerfile ./server
-docker built -t avis2good/multi-worker:latest -t avis2good/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t avis2good/multi-client:latest -t avis2good/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t avis2good/multi-server:latest -t avis2good/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t avis2good/multi-worker:latest -t avis2good/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push avis2good/multi-client:latest 
 docker push avis2good/multi-server:latest 
